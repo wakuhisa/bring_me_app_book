416963e07a1a4cd33225d75f8ae637d3740cbee8
wakuhisa <1bpj2117@gmail.com>
2020-10-06 21:03:05 +0900

    add validates :image, :title, :text, presence: true

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :text, presence: true
   mount_uploader :image, ImageUploader
 end
