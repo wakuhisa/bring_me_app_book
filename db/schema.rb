da2e0d249feb600b6acda996c2b233debcae980e
wakuhisa <1bpj2117@gmail.com>
2020-10-06 21:06:12 +0900

    do rails g migration AddNameToUsers name:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_111850) do
+ActiveRecord::Schema.define(version: 2020_10_06_114640) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image"
+    t.string "title"
+    t.text "text"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
