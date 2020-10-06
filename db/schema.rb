15b2f0fa5f2e7fa4e8df8d9cb10dda1f30bb0e78
wakuhisa <1bpj2117@gmail.com>
2020-10-06 22:00:49 +0900

    do rails g migration add_user_id_to_posts user_id:integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_123517) do
+ActiveRecord::Schema.define(version: 2020_10_06_125954) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_10_06_123517) do
     t.text "image"
     t.string "title"
     t.text "text"
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
