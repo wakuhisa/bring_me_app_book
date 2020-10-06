8b2671efda80c6ce1227a4e7f062deca8f028738
wakuhisa <1bpj2117@gmail.com>
2020-10-06 22:05:04 +0900

    add user, post, comment to has_many, belongs_to

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ApplicationRecord
   validates :image, presence: true
   validates :title, presence: true
   validates :text, presence: true
+  belongs_to :user
+  has_manu :comments
   mount_uploader :image, ImageUploader
 end
