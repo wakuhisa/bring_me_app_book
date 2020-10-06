194a94b0ad9f4abab2808ad7c6f9f3ac13dae771
wakuhisa <1bpj2117@gmail.com>
2020-10-06 20:20:22 +0900

    do rails g model Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_055106) do
+ActiveRecord::Schema.define(version: 2020_10_06_111850) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
