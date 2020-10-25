4ea3cb7087ce7b1cd55fad8fd2f0f222260ddb8c
wakuhisa <1bpj2117@gmail.com>
2020-10-25 18:30:31 +0900

    add def self.search(search)

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,9 @@ class Post < ApplicationRecord
   belongs_to :user, optional: true
   has_many :comments
   mount_uploader :img, ImgUploader
+
+  def self.search(search)
+    return Post.all unless search
+    Post.where('text LIKE(?)', "%#{search}%")
+  end
 end
