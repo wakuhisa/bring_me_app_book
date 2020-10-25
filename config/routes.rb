9838d25f69be75bc45ec661e77f37128ab6e8251
wakuhisa <1bpj2117@gmail.com>
2020-10-25 18:41:48 +0900

    add namespace :posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
+  namespace :posts do
+    resources :searches, only: :index
+  end
   resources :posts
   resources :users, only: :show
 end
