23ce5820a03ef2a2159778578687c8f072ae99c8
wakuhisa <1bpj2117@gmail.com>
2020-10-26 01:59:19 +0900

    edit def create & comment_params to comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comment_params)
-    redirect_to "/posts/#{comment.post.id}"
+    redirect_to "/posts/#{@comment.post.id}"
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:post).maege(user_id: current_user.id, post_id: params[:post_id])
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
   end
 end
