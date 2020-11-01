dc2335d451ede7d66a10200ce37a064e780e9114
wakuhisa <1bpj2117@gmail.com>
2020-11-02 01:08:26 +0900

    rspec describe User#create change FactoryBot.build to user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,29 +3,18 @@ require 'rails_helper'
 RSpec.describe User do
   describe '#create' do
     it "名前、メール、パスワードがある場合、有効である" do
-      user = User.new(
-        name: "testman",
-        email: "testman@example.com",
-        password: "password"
-      )
+      user = FactoryBot.build(:user)
       expect(user).to be_valid
     end
     it "名前がない場合、無効である" do
-      user = User.new(
-        name: nil,
-        email: "testman@example.com",
-        password: "password"
-      )
+      user = FactoryBot.build(:user, name: nil)
       user.valid?
       expect(user.errors[:name]).to include("can't be blank")
     end
     it "メールアドレスがない場合、無効である" do
-      user = User.new(
-        name: "testman",
-        email: nil,
-        password: "password"
-      )
-      expect(user.valid?).to eq(false)
+      user = FactoryBot.build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
     end
     it "重複したメールアドレスの場合、無効である" do
       FactoryBot.create(:user, email: "test@mail.com")
@@ -34,12 +23,9 @@ RSpec.describe User do
       expect(user.errors[:email]).to include("has already been taken")
     end
     it "パスワードがない場合、無効である" do
-      user = User.new(
-        name: "testman",
-        email: "testman@example.com",
-        password: nil
-      )
-      expect(user.valid?).to eq(false)
+      user = FactoryBot.build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
     end
   end
 end
