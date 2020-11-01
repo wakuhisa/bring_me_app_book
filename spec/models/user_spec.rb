6810514d4170b3310d68dad243d45362496412d1
wakuhisa <1bpj2117@gmail.com>
2020-11-02 00:34:20 +0900

    add rspec describe User#create to user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User do
+  describe '#create' do
+    it "名前、メール、パスワードがある場合、有効である" do
+      user = User.new(
+        name: "testman",
+        email: "testman@example.com",
+        password: "password"
+      )
+      expect(user).to be_valid
+    end
+    it "名前がない場合、無効である" do
+      user = User.new(
+        name: nil,
+        email: "testman@example.com",
+        password: "password"
+      )
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+    it "メールアドレスがない場合、無効である" do
+      user = User.new(
+        name: "testman",
+        email: nil,
+        password: "password"
+      )
+      expect(user.valid?).to eq(false)
+    end
+    it "重複したメールアドレスの場合、無効である" do
+      FactoryBot.create(:user, email: "test@mail.com")
+      user = FactoryBot.build(:user, email: "test@mail.com")
+      user.valid?
+      expect(user.errors[:email]).to include("has already been taken")
+    end
+    it "パスワードがない場合、無効である" do
+      user = User.new(
+        name: "testman",
+        email: "testman@example.com",
+        password: nil
+      )
+      expect(user.valid?).to eq(false)
+    end
+  end
 end
